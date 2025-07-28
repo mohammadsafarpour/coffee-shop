17fb45341a27eb2cabfd63e3a5bf113e2e303d6a
test1 <mohammadsafarpour112@gmail.com>
2025-07-28 18:57:00 +0330

    Safarpour first revise

--- products/models.py
+++ products/models.py
@@ -3,6 +3,15 @@ from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name_plural = 'Categories'
+        verbose_name = 'Category'
 
     def __str__(self):
         return self.name
@@ -16,9 +25,15 @@ class Ingredient(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
+
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
     ingredient = models.ManyToManyField(Ingredient, related_name='products')
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -26,6 +41,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ('name',)
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['-created']),
+        ]
+        verbose_name_plural = 'Products'
+        verbose_name = 'Product'
 
 
 class ProductImage(models.Model):
