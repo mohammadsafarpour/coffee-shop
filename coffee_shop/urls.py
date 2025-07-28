9b2124426cf24630a2f5e0e2c7516755811b30cc
Mohammad Yousefi <95502880+MohammadYR@users.noreply.github.com>
2025-07-28 22:06:42 +0330

    Add logout page and update login template and URL routing

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -19,4 +19,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # previous login url
+    # path('login/', views.user_login, name='login'),
+    # login / logout urls
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
