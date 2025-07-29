e32f098df94826dd9bfee1c72fee4a88a4686cd4
yousef <yousef.mokhtar1384@gmail.com>
2025-07-29 23:17:21 +0330

    admin_added_order

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import OrderItem, Order
 
-# Register your models here.
+class OrderAdminInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ('price',)
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'created_at', 'status']
+    list_filter = ['customer', 'created_at', 'status']
+    
