bdcb4e66caa54edea40ca883caf88ae9175cf330
yousef <yousef.mokhtar1384@gmail.com>
2025-07-30 20:46:05 +0330

    fixed_order

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Order, OrderItem
 
-# Create your views here.
+@login_required
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'orders/list.html', {'orders': orders})
