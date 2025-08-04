914cdbf82099049706104253293011ecf17a390a
yousef <yousef.mokhtar1384@gmail.com>
2025-08-04 20:12:20 +0330

    decimal_places_edited

--- orders/models.py
+++ orders/models.py
@@ -35,7 +35,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0, null=True, blank=True)
     quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
