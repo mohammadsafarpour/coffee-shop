88bec253da36e08bad0d86a442d97cd728dceb81
Mohammad Yousefi <95502880+MohammadYR@users.noreply.github.com>
2025-08-06 21:36:12 +0330

    update notification app and templates

--- notification/urls.py
+++ notification/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.notification_list, name='notification-list'),
+    path('mark-as-read/<int:notification_id>/', views.mark_notification_as_read, name='mark-notification-as-read'),
 ]
--- notification/views.py
+++ notification/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Notification
 
+# def notification_list(request):
+#     notification = Notification.objects.filter(recipient=request.user).order_by('-created_at')
+#     return render(request, 'notification/notification_list.html', {'notification': notification})
+
+
+@login_required
 def notification_list(request):
-    notification = Notification.objects.filter(recipient=request.user).order_by('-created_at')
-    return render(request, 'notification/notification_list.html', {'notification': notification})
+    notifications = Notification.objects.filter(user=request.user)
+    return render(request, 'notifications/notification_list.html', {'notifications': notifications})
+
+@login_required
+def mark_notification_as_read(request, notification_id):
+    notification = get_object_or_404(Notification, id=notification_id, user=request.user)
+    notification.is_read = True
+    notification.save()
+    return redirect('notification_list')
+
