9601a1a794e04f935d78fff1a87850684951e7cd
mohammadsafarpour <mohammadsafarpour112@gmail.com>
2025-08-22 14:37:30 +0330

    final revised before first phase presentation

--- orders/api_urls.py
+++ orders/api_urls.py
@@ -1,9 +1,9 @@
-from rest_framework.routers import DefaultRouter
-from .api_views import OrderHistoryViewset
+# from rest_framework.routers import DefaultRouter
+# from .api_views import OrderHistoryViewset
 
-router = DefaultRouter()
-router.register('history', OrderHistoryViewset, basename='order-history')
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register('history', OrderHistoryViewset, basename='order-history')
+# urlpatterns = router.urls
 
 # By normal url: 
 # from django.urls import path
@@ -12,3 +12,10 @@ urlpatterns = router.urls
 # urlpatterns = [
 #     path('history/', OrderHistoryAPIView.as_view(), name='order-history-api'),
 # ]
+
+from rest_framework.routers import DefaultRouter
+from .api_views import OrderHistoryViewSet
+
+router = DefaultRouter()
+router.register('history', OrderHistoryViewSet, basename='order-history')
+urlpatterns = router.urls
