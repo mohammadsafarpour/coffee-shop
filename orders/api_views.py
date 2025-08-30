1dd664d502cfe2ec4f76a5cb6e40849871fecdd8
mohammadsafarpour <mohammadsafarpour112@gmail.com>
2025-08-30 21:40:25 +0330

    JWT auth added to project (authorization is upgraded)

--- orders/api_views.py
+++ orders/api_views.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -6,6 +7,7 @@ from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from drf_spectacular.utils import extend_schema
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+
 from products.models import Product
 from .models import Order, OrderItem
 from .serializers import (
